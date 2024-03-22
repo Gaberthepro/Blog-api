8265d85f5e01e231a28ee974971d874be59a8d24
GaberČuješ <gabercujes@gmail.com>
2024-03-22 10:49:56 +0100

    generate resource post

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://gabercujes:fYq0uktC0d3UQnwR@blog.9hdakid.mongodb.net/',
+      {},
+    ),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
