f23081c68f8171468d744c4dd94f3696f0cf80d9
GaberČuješ <gabercujes@gmail.com>
2024-03-22 11:46:42 +0100

    mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb://localhost/blog',
-      {},
-    ),
-    PostsModule,
-  ],
+  imports: [MongooseModule.forRoot('mongodb+srv://gabercujes:j5727zLIElUvV5cN@blog.9hdakid.mongodb.net/'), PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
