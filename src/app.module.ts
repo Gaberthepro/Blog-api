29373ea4888b2bfd7bbf07c059edad10a5ec055b
GaberČuješ <gabercujes@gmail.com>
2024-03-22 11:03:29 +0100

    Schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { PostsModule } from './posts/posts.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://gabercujes:fYq0uktC0d3UQnwR@blog.9hdakid.mongodb.net/',
+      'mongodb://localhost/blog',
       {},
     ),
     PostsModule,
