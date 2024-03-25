fd8bc02407c06e49475ffc2ab2eb59fdf4dbda73
GaberČuješ <gabercujes@gmail.com>
2024-03-25 13:46:24 +0100

    Cors

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Header } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
@@ -8,7 +8,8 @@ export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
+  create(@Req() request: Request, @Body() createPostDto: CreatePostDto) {
+    console.log(request.body)
     return this.postsService.create(createPostDto);
   }
 
