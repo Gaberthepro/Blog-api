eed779e560c231b1db175e487ec165a94f71d860
GaberČuješ <gabercujes@gmail.com>
2024-03-26 17:57:33 +0100

    bcrypt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,15 +4,21 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { Users } from './schemas/users.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel('Users') private readonly usersModel: Model<Users>,
   ) {}
-  create(createUserDto: CreateUserDto) {
-    const createdPost = new this.usersModel(createUserDto);
-    return createdPost.save();
+  async create(createUserDto: CreateUserDto) {
+    const saltOrRounds = 10;
+    const createdUser = new this.usersModel(createUserDto);
+    createdUser.password = await bcrypt.hash(
+      createdUser.password,
+      saltOrRounds,
+    );
+    return createdUser.save();
   }
 
   findAll() {
